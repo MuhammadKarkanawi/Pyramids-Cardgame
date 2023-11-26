be3315939e5e2ea43eccb557a32f2b450f3c4445
mohammadkarkanawi <mohammad.karkanwi@tu-dortmund.de>
2023-11-26 23:46:09 +0100

    tests verbessert
    Signed-off-by: mohammadkarkanawi <mohammad.karkanwi@tu-dortmund.de>

--- src/test/kotlin/service/TestGameService.kt
+++ src/test/kotlin/service/TestGameService.kt
@@ -6,16 +6,16 @@ import view.Refreshable
 class GameServiceTest {
 
 
-   private fun setUpGame(vararg refreshables: Refreshable) : RootService{
+    private fun setUpGame(vararg refreshables: Refreshable): RootService {
         val rootService = RootService()
-        val gameService= rootService.gameService
-        val currentGame= rootService.currentGame
+        val gameService = rootService.gameService
+        val currentGame = rootService.currentGame
         checkNotNull(currentGame)
         refreshables.forEach { rootService.addRefreshable(it) }
 
         val pyramidCards = listOf(
 
-            // player 1 left bottom of stack
+
             Card(CardSuit.CLUBS, CardValue.QUEEN),
             Card(CardSuit.SPADES, CardValue.TEN),
             Card(CardSuit.DIAMONDS, CardValue.SEVEN),
@@ -47,7 +47,8 @@ class GameServiceTest {
             Card(CardSuit.CLUBS, CardValue.TEN),
             Card(CardSuit.DIAMONDS, CardValue.TEN),
             Card(CardSuit.HEARTS, CardValue.JACK),
-            Card(CardSuit.SPADES, CardValue.KING))
+            Card(CardSuit.SPADES, CardValue.KING)
+        )
 
 
         val drawStackCards = listOf(
@@ -76,7 +77,8 @@ class GameServiceTest {
             Card(CardSuit.HEARTS, CardValue.FIVE),
             Card(CardSuit.SPADES, CardValue.SIX),
             Card(CardSuit.CLUBS, CardValue.SIX),
-            Card(CardSuit.HEARTS, CardValue.SIX))
+            Card(CardSuit.HEARTS, CardValue.SIX)
+        )
 
 
         var i = 0
@@ -89,164 +91,189 @@ class GameServiceTest {
                 i++
             }
         }
-        currentGame.pyramid.cards=pyramid
-        currentGame.drawStack.cards.addAll(drawStackCards)
 
         gameService.startGame("Bob", "Alice")
+
+        currentGame.pyramid.cards = pyramid
+        currentGame.drawStack.cards.addAll(drawStackCards)
+
+
         println(currentGame)
         return rootService
     }
-        @Test
-        fun testStartGame() {
-            val refreshableTest = RefreshableTest()
-            val rootService =setUpGame(refreshableTest)
-            val gameService= rootService.gameService
-            val currentGame = rootService.currentGame
-            assertNotNull(currentGame)
 
+    @Test
+    fun testStartGame() {
+        val refreshableTest = RefreshableTest()
+        val mc =RootService()
+        assertNull(mc.currentGame)
 
-            assertFalse(refreshableTest.refreshAfterStartGameCalled)
+        assertFalse(refreshableTest.refreshAfterStartGameCalled)
+        refreshableTest.reset()
 
-            refreshableTest.reset()
+        val rootService =setUpGame(refreshableTest)
+        val gameService = rootService.gameService
+        assertTrue(refreshableTest.refreshAfterStartGameCalled)
 
-            gameService.startGame("Player1", "Player2")
-            assertTrue(refreshableTest.refreshAfterStartGameCalled)
+        assertEquals("Player1", gameService.player1.name)
+        assertEquals("Player2", gameService.player2.name)
 
-            assertEquals("Player1", gameService.player1.name)
-            assertEquals("Player2", gameService.player2.name)
+        // Verify that the game pyramid is created and initialized
+        assertNotNull(rootService.currentGame)
+        assertNotNull(rootService.currentGame?.pyramid)
+        assertEquals(24, rootService.currentGame?.drawStack?.cards?.size)
+        assertEquals(7, rootService.currentGame?.pyramid?.cards?.size)
+        assertEquals(3, rootService.currentGame?.pyramid?.cards!![2].size)
+        gameService.endGame()
+        refreshableTest.reset()
 
-            // Verify that the game pyramid is created and initialized
-            assertNotNull(rootService.currentGame)
-            assertNotNull(rootService.currentGame?.pyramid)
-            assertEquals(24, rootService.currentGame?.drawStack?.cards?.size)
-            assertEquals(7, rootService.currentGame?.pyramid?.cards?.size)
-            assertEquals(3, rootService.currentGame?.pyramid?.cards!![3].size)
-            gameService.endGame()
-            refreshableTest.reset()
-            assertFalse(refreshableTest.refreshAfterStartGameCalled)
-
-        }
-
-        @Test
-        fun testChangePlayer() {
-            val rootService = RootService()
-            val gameService= rootService.gameService
-            val currentGame = rootService.currentGame
-            assertNotNull(currentGame)
-            val refreshableTest = RefreshableTest()
-            rootService.addRefreshable(refreshableTest)
-
-
-            // Start a game to have a valid current game
-            gameService.startGame("Player1", "Player2")
-
-            assertFalse(refreshableTest.refreshAfterChangePlayerCalled)
-            refreshableTest.reset()
-
-            // Change the player and verify
-            val currentPlayerBeforeChange = rootService.currentGame?.currentPlayer
-            val newPlayer = gameService.changePlayer()
-
-            assertNotNull(currentPlayerBeforeChange)
-            assertNotNull(newPlayer)
-            assertNotEquals(currentPlayerBeforeChange, newPlayer)
-
-            gameService.changePlayer()
-            assertTrue(refreshableTest.refreshAfterChangePlayerCalled)
-
-        }
-
-        @Test
-        fun testCheckCardChoice() {
-            val rootService = RootService()
-            val gameService= rootService.gameService
-            val currentGame = rootService.currentGame
-            assertNotNull(currentGame)
-
-            val refreshableTest = RefreshableTest()
-            rootService.addRefreshable(refreshableTest)
-            // Start a game to have a valid current game
-            gameService.startGame("Player1", "Player2")
-
-            assertFalse(refreshableTest.refreshAfterRemovePairCalled)
-            refreshableTest.reset()
-
-
-            // Create two cards for testing
-            val card1 = Card(CardSuit.HEARTS, CardValue.ACE)
-            val card2 = Card(CardSuit.DIAMONDS, CardValue.TWO)
-
-            currentGame?.pyramid!!.cards[0][0] = card1
-            currentGame.pyramid.cards[1][0] = card2
-            // Check if the card choice is valid
-            val isValid = gameService.checkCardChoice(card1, card2)
-            assertTrue(isValid)
-            rootService.playerActionService.removePair(card1,card2)
-
-            assertTrue(refreshableTest.refreshAfterRemovePairCalled)
-        }
-
-        @Test
-        fun testEndGame() {
-            val rootService = RootService()
-            val gameService= rootService.gameService
-            val currentGame = rootService.currentGame
-            assertNotNull(currentGame)
-
-            val refreshableTest = RefreshableTest()
-            rootService.addRefreshable(refreshableTest)
-
-
-            // Start a game to have a valid current game
-            gameService.startGame("Player1", "Player2")
-
-            // Set scores for testing
-            currentGame!!.playerList[0].score = 10
-            currentGame.playerList[1].score = 15
-
-            assertFalse(refreshableTest.refreshAfterEndGameCalled)
-            refreshableTest.reset()
-            // End the game and verify the winner
-            gameService.endGame()
-            assertTrue(refreshableTest.refreshAfterEndGameCalled)
-
-        }
-
-        @Test
-        fun testFlipPyramidCards() {
-            val rootService = RootService()
-            val gameService= rootService.gameService
-            val refreshableTest = RefreshableTest()
-            val currentGame = rootService.currentGame
-            assertNotNull(currentGame)
-            rootService.addRefreshable(refreshableTest)
-
-
-            // Start a game to have a valid current game
-            gameService.startGame("Player1", "Player2")
-
-            assertFalse(refreshableTest.refreshAfterFlipCalled)
-            refreshableTest.reset()
-
-
-            // Create two cards for testing
-            val card1 = Card(CardSuit.HEARTS, CardValue.ACE)
-            val card2 = Card(CardSuit.DIAMONDS, CardValue.TWO)
-
-            currentGame?.pyramid!!.cards[0][0] = card1
-            currentGame.pyramid.cards[1][0] = card2
-
-            // Check if the card choice is valid
-            val isValid = gameService.checkCardChoice(card1, card2)
-            assertTrue(isValid)
-            rootService.playerActionService.removePair(card1,card2)
-
-            // Flip the pyramid cards and verify
-            gameService.flipPyramidCards()
-
-            assertTrue(refreshableTest.refreshAfterFlipCalled)
-
-            assertTrue(currentGame.pyramid.cards.all { it.first().isRevealed && it.last().isRevealed })
-
-        }
+        assertFalse(refreshableTest.refreshAfterStartGameCalled)
+        assertTrue(refreshableTest.refreshAfterEndGameCalled)
     }
+
+    @Test
+    fun testChangePlayer() {
+        val refreshableTest = RefreshableTest()
+        val rootService =setUpGame(refreshableTest)
+        val gameService = rootService.gameService
+        val currentGame = rootService.currentGame
+        assertNotNull(currentGame)
+
+        assertFalse(refreshableTest.refreshAfterChangePlayerCalled)
+        refreshableTest.reset()
+
+        gameService.changePlayer()
+        assertTrue(refreshableTest.refreshAfterChangePlayerCalled)
+        refreshableTest.reset()
+
+        gameService.changePlayer()
+        assertTrue(refreshableTest.refreshAfterChangePlayerCalled)
+        refreshableTest.reset()
+
+    }
+
+    @Test
+    fun testCheckCardChoice() {
+        val refreshableTest = RefreshableTest()
+        val rootService =setUpGame(refreshableTest)
+        val gameService = rootService.gameService
+        val currentGame = rootService.currentGame
+        assertNotNull(currentGame)
+
+        assertFalse(refreshableTest.refreshAfterRemovePairCalled)
+        refreshableTest.reset()
+
+        assertTrue( gameService.checkCardChoice(Card(CardSuit.DIAMONDS, CardValue.SEVEN),
+            Card(CardSuit.CLUBS, CardValue.EIGHT)))
+
+        assertFalse(refreshableTest.refreshAfterRemovePairCalled)
+        rootService.playerActionService.removePair(Card(CardSuit.DIAMONDS, CardValue.SEVEN),
+            Card(CardSuit.CLUBS, CardValue.EIGHT))
+        assertTrue(refreshableTest.refreshAfterRemovePairCalled)
+    }
+
+    @Test
+    fun testEndGame() {
+        val refreshableTest = RefreshableTest()
+        val rootService =setUpGame(refreshableTest)
+        val gameService = rootService.gameService
+        val currentGame = rootService.currentGame
+        assertNotNull(currentGame)
+
+        // Set scores for testing
+        currentGame!!.playerList[0].score = 10
+        currentGame.playerList[1].score = 15
+
+
+        assertEquals(10,currentGame.playerList[0].score)
+        assertEquals(15,currentGame.playerList[1].score)
+
+        assertFalse(refreshableTest.refreshAfterEndGameCalled)
+        refreshableTest.reset()
+        // End the game and verify the winner
+        gameService.endGame()
+        assertTrue(refreshableTest.refreshAfterEndGameCalled)
+
+    }
+
+    @Test
+    fun testFlipPyramidCards() {
+        val refreshableTest = RefreshableTest()
+        val rootService =setUpGame(refreshableTest)
+        val gameService = rootService.gameService
+        val currentGame = rootService.currentGame
+        assertNotNull(currentGame)
+
+
+        assertFalse(refreshableTest.refreshAfterRemovePairCalled)
+        refreshableTest.reset()
+
+        assertFalse(refreshableTest.refreshAfterFlipCalled)
+        refreshableTest.reset()
+
+        //valide cards from pyramid
+        assertTrue( gameService.checkCardChoice(Card(CardSuit.SPADES, CardValue.SEVEN),
+            Card(CardSuit.SPADES, CardValue.EIGHT)))
+
+        rootService.playerActionService.removePair(Card(CardSuit.SPADES, CardValue.SEVEN),
+            Card(CardSuit.SPADES, CardValue.EIGHT))
+
+        assertTrue(refreshableTest.refreshAfterRemovePairCalled)
+        assertTrue(refreshableTest.refreshAfterFlipCalled)
+
+        //not valide cards from pyramid
+        assertFalse( gameService.checkCardChoice(Card(CardSuit.CLUBS,CardValue.NINE),
+            Card(CardSuit.DIAMONDS, CardValue.NINE)))
+        refreshableTest.reset()
+
+        //valide cards from reserve & pyramide
+        assertTrue( gameService.checkCardChoice(Card(CardSuit.SPADES, CardValue.KING),
+            Card(CardSuit.SPADES, CardValue.ACE)))
+
+        rootService.playerActionService.removePair(Card(CardSuit.SPADES, CardValue.KING),
+            Card(CardSuit.SPADES, CardValue.ACE))
+
+        assertTrue(refreshableTest.refreshAfterRemovePairCalled)
+        assertTrue(refreshableTest.refreshAfterFlipCalled)
+
+
+        assertTrue(currentGame!!.pyramid.cards.all { it.first().isRevealed && it.last().isRevealed })
+
+    }
+
+    @Test
+    fun testPass() {
+        val refreshableTest = RefreshableTest()
+        val rootService =setUpGame(refreshableTest)
+        val currentGame = rootService.currentGame
+        assertNotNull(currentGame)
+
+        assertFalse(refreshableTest.refreshAfterPassCalled)
+        refreshableTest.reset()
+
+        rootService.playerActionService.pass()
+        assertTrue(refreshableTest.refreshAfterStartGameCalled)
+
+        rootService.playerActionService.pass()
+        assertTrue(refreshableTest.refreshAfterEndGameCalled)
+
+    }
+
+    @Test
+    fun testRevealCard() {
+        val refreshableTest = RefreshableTest()
+        val rootService =setUpGame(refreshableTest)
+        val currentGame = rootService.currentGame
+        assertNotNull(currentGame)
+
+        assertFalse(refreshableTest.refreshAfterRevealCardCalled)
+        refreshableTest.reset()
+
+        rootService.playerActionService.revealCard()
+
+        assertNotEquals(Card(CardSuit.HEARTS, CardValue.SIX),currentGame!!.drawStack.cards[0])
+        assertEquals(Card(CardSuit.HEARTS, CardValue.SIX), currentGame.reserveStack.cards[0] )
+
+        assertTrue(refreshableTest.refreshAfterRevealCardCalled)
+    }
+}
