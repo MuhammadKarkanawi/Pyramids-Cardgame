199f05fe27ecb0f7b16fa50549ea705448a4e631
mohammadkarkanawi <mohammad.karkanwi@tu-dortmund.de>
2023-11-27 00:01:01 +0100

    tests verbessert
    Signed-off-by: mohammadkarkanawi <mohammad.karkanwi@tu-dortmund.de>

--- src/test/kotlin/service/Playertest.kt
+++ src/test/kotlin/service/Playertest.kt
@@ -2,38 +2,7 @@ package service
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import entity.Player
-
-/**class Playertest {
-    @Test
-    fun creatPlayerCaseOne () {
-// Testdaten erzeugen
-        val playerName = "john"
-        var playerScore =0
-
-        // Zu testende Methode mit Testdaten aufrufen
-        var createdPlayer: Player = Player(playerName)
-// Test, ob die Werte im neuen Modul mit den übergebenen wert übereinstimmt
-        assertEquals (playerName , createdPlayer.name)
-        assertEquals (playerScore, createdPlayer.score )
-
-        /**
-         * Testet, dass die Erstellung eines Moduls mit leerem Namen fehlschl gt */
-        @Test
-        fun creatPlayerCaseTwo () {
-// Testdaten erzeugen
-            val playerName = ""
-            var playerScore = 0
-// Test: create Module schlägtfehl
-            assertFailsWith<IllegalArgumentException> {
-                // Zu testende Methode mit Testdaten aufrufen
-                var createdPlayer: Player = Player(playerName)
-            }
-        }
-    }
-}
- */
-
-
+import org.junit.jupiter.api.Assertions.assertNotEquals
 
 
 class PlayerTest {
@@ -43,7 +12,7 @@ class PlayerTest {
         val playerName = "Alice"
         val player = Player(playerName)
 
-        assertEquals(playerName, player.name)
+        assertEquals("Alice", player.name)
         assertEquals(0, player.score)
     }
 
@@ -53,7 +22,8 @@ class PlayerTest {
 
         // Increment the score
         player.score += 10
-
+        assertNotEquals("Alice", player.name)
+        assertEquals("Bob", player.name)
         assertEquals(10, player.score)
     }
 }
